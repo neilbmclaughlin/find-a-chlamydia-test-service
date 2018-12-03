f3b765965ab970fe3be3dd37b8198663eb052567
neil mclaughlin <neil.mclaughlin@digital.nhs.uk>
2018-12-03 09:18:48 +0000

    Move queries to specific subdirectories

--- refresh.sh
+++ refresh.sh
@@ -18,8 +18,8 @@ do
     lon=$(cut -f3 -d' ' <<< "${lat_lon}")
 
     sed_subs="s/_SIZE_/${size}/g;s/_LAT_/${lat}/g;s/_LON_/${lon}/g"
-    as_query=$(sed "${sed_subs}" as-${i}-query.json)
-    es_query=$(sed "${sed_subs}" es-${i}-query.json)
+    as_query=$(sed "${sed_subs}" queries/as/${i}.json)
+    es_query=$(sed "${sed_subs}" queries/es/${i}.json)
     curl -s -XPOST "https://nhsapidev.azure-api.net/service-search/search?api-version=1" -H "Content-Type: application/json" -H "subscription-key: ${API_SUB_KEY}" -d "${as_query}" | jq '.' > as-${i}-results.json
     curl -s  -XPOST https://es.staging.beta.nhschoices.net/sexual-health-services/_search -H 'Content-Type: application/json' -H 'cache-control: no-cache' -d "${es_query}" | jq '.' > es-${i}-results.json
     jq -C '.value[] | if .NACSCode then .NACSCode else .OrganisationID end' as-${i}-results.json | sort > as-${i}-id-sorted-list.json
