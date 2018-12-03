ed18b2c8bc82b4b308ddf34a0b90d22b99dcd6f1
neil mclaughlin <neil.mclaughlin@digital.nhs.uk>
2018-12-03 09:28:05 +0000

    Move results to specific directories

--- refresh.sh
+++ refresh.sh
@@ -20,11 +20,11 @@ do
     sed_subs="s/_SIZE_/${size}/g;s/_LAT_/${lat}/g;s/_LON_/${lon}/g"
     as_query=$(sed "${sed_subs}" queries/as/${i}.json)
     es_query=$(sed "${sed_subs}" queries/es/${i}.json)
-    curl -s -XPOST "https://nhsapidev.azure-api.net/service-search/search?api-version=1" -H "Content-Type: application/json" -H "subscription-key: ${API_SUB_KEY}" -d "${as_query}" | jq '.' > as-${i}-results.json
-    curl -s  -XPOST https://es.staging.beta.nhschoices.net/sexual-health-services/_search -H 'Content-Type: application/json' -H 'cache-control: no-cache' -d "${es_query}" | jq '.' > es-${i}-results.json
-    jq -C '.value[] | if .NACSCode then .NACSCode else .OrganisationID end' as-${i}-results.json | sort > as-${i}-id-sorted-list.json
-    jq -C '.hits.hits[]._source | if .odsCode then .odsCode else .gsdId end' es-${i}-results.json | sort > es-${i}-id-sorted-list.json
-    sdiff -l  as-${i}-id-sorted-list.json es-${i}-id-sorted-list.json | cat -n | grep -v -e '($'
+    curl -s -XPOST "https://nhsapidev.azure-api.net/service-search/search?api-version=1" -H "Content-Type: application/json" -H "subscription-key: ${API_SUB_KEY}" -d "${as_query}" | jq '.' > results/as/${i}.json
+    curl -s  -XPOST https://es.staging.beta.nhschoices.net/sexual-health-services/_search -H 'Content-Type: application/json' -H 'cache-control: no-cache' -d "${es_query}" | jq '.' > results/es/${i}.json
+    jq -C '.value[] | if .NACSCode then .NACSCode else .OrganisationID end' results/as/${i}.json | sort > results/as/${i}-id-sorted-list.json
+    jq -C '.hits.hits[]._source | if .odsCode then .odsCode else .gsdId end' results/es/${i}.json | sort > results/es/${i}-id-sorted-list.json
+    sdiff -l  results/as/${i}-id-sorted-list.json results/es/${i}-id-sorted-list.json | cat -n | grep -v -e '($'
     # diff -y --suppress-common-lines as-${i}-id-sorted-list.json es-${i}-id-sorted-list.json
   done
 done
